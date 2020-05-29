5441aa9e5a98ae1ee55bc07a3ece5ddd55e505e4
beatriz-perez <beatriz.pfp@gmail.com>
2020-05-29 17:26:55 +0200

    update

---
+++ docs/precache-manifest.be015db87c46cdd5ad53b3a42ee4ded4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4287b0e057d8586aec645f653407783",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e5d8e57795c8aca542a",
+    "url": "./static/css/main.5f189722.chunk.css"
+  },
+  {
+    "revision": "4deffc4fbca7d24f71d3",
+    "url": "./static/js/2.adf66301.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.adf66301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e5d8e57795c8aca542a",
+    "url": "./static/js/main.2353d1c2.chunk.js"
+  },
+  {
+    "revision": "b534f079d1572efbf673",
+    "url": "./static/js/runtime-main.62779cc3.js"
+  },
+  {
+    "revision": "c623a7a8cf04ad4182308759294d8064",
+    "url": "./static/media/CV-Beatriz-Perez-FrontEnd.c623a7a8.pdf"
+  },
+  {
+    "revision": "c5ec03df2b2cb5329d593711a05581fc",
+    "url": "./static/media/CV.c5ec03df.jpg"
+  },
+  {
+    "revision": "7e9ef1bf3c3f0525ce26257639e95f1b",
+    "url": "./static/media/LinkedInCV.7e9ef1bf.jpeg"
+  },
+  {
+    "revision": "29d6da613cd589447ee164a54938a4f4",
+    "url": "./static/media/Resume-Beatriz-Perez FrontEnd.29d6da61.pdf"
+  },
+  {
+    "revision": "cd5642cd8ea7b97adec61443f4262162",
+    "url": "./static/media/Soulcraft.cd5642cd.ttf"
+  },
+  {
+    "revision": "95b885ace35426b5b2577978a2c87444",
+    "url": "./static/media/cv_picture.95b885ac.jpeg"
+  }
+]);
